118d3b34857017be322a6d7e49181f48006772cb
djadji-gueye <sennegueye@gmail.com>
2026-04-29 14:47:14 +0000

    seo yitewo

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'www.yitewo.com' }],
+        destination: 'https://yitewo.com/:path*',
+        permanent: true, // 301 — bon pour le SEO
+      },
+    ];
+  },
 };
 
 export default nextConfig;
